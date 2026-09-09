267acf4c1e849108e8eccd4807a710e1f65aaef8
kaua <kauaxavier032@gmail.com>
2026-09-08 22:29:10 -0300

    feat: add .gitignore file to ignore unnecessary files

---
+++ src/main/java/com/tradeflow/model/WalletItem.java
@@ -0,0 +1,33 @@
+package com.tradeflow.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "wallet_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private Wallet wallet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "asset_id", nullable = false)
+    private Asset asset;
+
+    @Column(nullable = false, precision = 15, scale = 4)
+    private BigDecimal quantity;
+}
