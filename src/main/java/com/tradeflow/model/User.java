19490ff39d6ca3e4858d02bfd18b7266b4707476
kaua <kauaxavier032@gmail.com>
2026-09-08 22:09:05 -0300

    feat: create User entity with attributes and JPA configurations

---
+++ src/main/java/com/tradeflow/model/User.java
@@ -0,0 +1,34 @@
+package com.tradeflow.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users") // "users" no plural para evitar conflito com a palavra reservada do Postgres
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String document; // Representando o CPF para validações futuras
+
+    // O relacionamento com a Carteira (Wallet) será feito assim que criarmos a entidade Wallet
+}
