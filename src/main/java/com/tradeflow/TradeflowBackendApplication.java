eb70435154e6839c18ac028584e28df77f032347
Kauã Xavier <104097066+Kaua032@users.noreply.github.com>
2026-09-08 21:21:34 -0300

    Add files via upload

---
+++ src/main/java/com/tradeflow/TradeflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tradeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeflowBackendApplication.class, args);
+	}
+
+}
